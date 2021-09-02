f9c52903f8180f5c52821c615c5a774647c86cdb
ismail can <ismailcankaratasss@gmail.com>
2021-09-02 06:59:09 -0700

    Euro ve Gram Altın eklendi.

--- assets/main.js
+++ assets/main.js
@@ -2,12 +2,24 @@ let data = fetch("https://finans.truncgil.com/today.json")
 .then(response=>response.json())
 .then(veri=>{
 $(".guncel").html("Güncelleme Tarihi : " + veri["Update_Date"]);
+$(".usd").html("Dolar " + veri["USD"].Alış + "₺");
+$(".eur").html("Euro " + veri["EUR"].Alış + "₺");
+$(".gram-altin").html("Gran Altın " + veri["gram-altin"].Alış + "₺");
 
+ $("#doviz").on("change", function() {
+  var secilenDoviz = $("#doviz").val();
+  var miktar = $(".miktar").val();
+  var dolar = veri[secilenDoviz].Alış.replace(",",".");
+  var dolarHesap = parseFloat(miktar) * parseFloat(dolar) ;
+  $(".dolar").html(parseFloat(dolarHesap) + "₺");
+
+ })
 $(".miktar").keyup(function() {
+var secilenDoviz = $("#doviz").val();
 var miktar = $(".miktar").val();
-var dolar = veri["USD"].Alış.replace(",",".");
+var dolar = veri[secilenDoviz].Alış.replace(",",".");
 var dolarHesap = parseFloat(miktar) * parseFloat(dolar) ;
-$(".dolar").html(parseFloat(dolarHesap));
+$(".dolar").html(parseFloat(dolarHesap) + "₺");
 
 })
 });
